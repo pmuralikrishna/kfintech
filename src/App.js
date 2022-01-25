edc58ca3c3120ff24cf27ad33a3bdf4dbafaf991
murali <pmuralikrishna29@gmail.com>
2022-01-25 20:50:01 +0530

    Add login page and integrate login.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import WelcomePage from './pages/WelcomePage/WelcomePage';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
@@ -14,7 +15,12 @@ function App() {
           >
             <WelcomePage />
           </Route>
-
+          <Route
+            exact={true}
+            path="/login"
+          >
+            <Login />
+          </Route>
         </Switch>
       </BrowserRouter>
     </div>
